35dea532c95cf6083f81accbf280b6b69145f7ec
afgbloch <bloch.aurelien@gmail.com>
2018-02-24 08:48:00 +0100

    dumb create_part

--- src/create_part.py
+++ src/create_part.py
@@ -1,10 +1,4 @@
 import note
 
 def create_part(files):
-	res = []
-	# for f in files:
-	# 	res.append(note.get_note(f))
-	return res
-
-# for i in create_part("../test"):
-# 	print(i)
+	notes = note.get_notes(files)
