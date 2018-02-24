0fa337cc808f6a3018932579108de90e35f6ac3f
afgbloch <bloch.aurelien@gmail.com>
2018-02-24 10:45:33 +0100

    create script

--- src/create_part.py
+++ src/create_part.py
@@ -1,4 +1,42 @@
 import note
+import io
+
+note_dic = {
+	"la" : "a",
+	"si" : "b",
+	"do" : "c",
+	"re" : "d",
+	"mi" : "e",
+	"fa" : "f",
+	"sol": "g",
+}
+rythme_dic = {
+	"half"      : 2,
+	"quarter"   : 4,
+	"sixteenth" : 6,
+	"eighth"    : 8,
+}
 
 def create_part(files):
 	notes = note.get_notes(files)
+	part = open("part.ly", "w")
+	part.write("""\\version "2.18.2"
+
+global= {
+  \key c \major
+  \\time 4/4
+}
+
+violinSolo= \\new Voice \\relative a' {
+""")
+	for n in notes:
+		part.write(note_dic[n[0]] + str(rythme_dic[n[1]]) + " ")
+	part.write("""
+}
+
+\score {
+    \\new Staff << \global \\violinSolo >>
+  \layout { }
+}""")
+
+
