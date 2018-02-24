1d570b20dff3fe3c67a732af4863ceb0c40c69d1
Martin Vassor <martin.vassor@epfl.ch>
2018-02-24 02:19:07 +0100

    Add variation based note detection + immune window

--- src/separator.py
+++ src/separator.py
@@ -1,9 +1,11 @@
 from PIL import Image
 
 window_width = 10
+threshold = 10
+immune = 10
 
 try: 
-    myImage = Image.open("../assets/extract.png")
+    myImage = Image.open("../assets/extract2.png")
     myImage.load()
 except:
     print "Cannot open image"
@@ -17,12 +19,24 @@ scaled.show()
 print "The thumbnail is:"
 print(scaled.format, scaled.size, scaled.mode)
 
+cur_sum = 0
+increasing=False
+next_open_slot=0
 for i in range(scaled.size[0]):
-    sum = 0
+    prev_sum = cur_sum
+    cur_sum = 0
     for j in range(scaled.size[1]):
         if scaled.getpixel((i, j)) < 64:
-            sum = sum + 1
-    print sum
-    if sum > 15:
-        small = scaled.crop((i, 0, i + window_width, 48))
-        small.save("../tmp/" + str(i) + ".png")
+            cur_sum = cur_sum + 1
+    print cur_sum
+    if cur_sum > prev_sum:
+        increasing=True
+    else:
+        if increasing == True and prev_sum > threshold and i >= next_open_slot:
+            small = scaled.crop((i-window_width/2, 0, i + window_width/2, 48))
+            small.save("../assets/separated/extract2/" + str(i).zfill(3) + ".png")
+            next_open_slot=i+immune
+
+        increasing=False
+
+
