4d095820bf22e6097715cf319c76904aec4c5e16
pokitoz <florian.depraz@epfl.ch>
2018-02-24 17:55:53 +0000

    Add script

--- template/start_.bash
+++ template/start_.bash
@@ -7,13 +7,30 @@ if [ ${1+x} ]; then
 
 python -m scripts.retrain \
   --bottleneck_dir=tf_files/bottlenecks \
-  --how_many_training_steps=500 \
+  --how_many_training_steps=1000 \
   --model_dir=tf_files/models/ \
   --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
   --output_graph=tf_files/retrained_graph.pb \
   --output_labels=tf_files/retrained_labels.txt \
   --architecture="${ARCHITECTURE}" \
-  --image_dir=../image_base
+  --image_dir=../sain
+
+cp tf_files/retrained_graph.pb ../src/retrained_graph_note.pb
+cp tf_files/retrained_labels.txt ../src/retrained_labels_note.txt
+
+python -m scripts.retrain \
+  --bottleneck_dir=tf_files/bottlenecks \
+  --how_many_training_steps=1000 \
+  --model_dir=tf_files/models/ \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --output_graph=tf_files/retrained_graph.pb \
+  --output_labels=tf_files/retrained_labels.txt \
+  --architecture="${ARCHITECTURE}" \
+  --image_dir=../image_rythme
+
+cp tf_files/retrained_graph.pb ../src/retrained_graph_rythme.pb
+cp tf_files/retrained_labels.txt ../src/retrained_labels_rythme.txt
+
 
 fi
   
