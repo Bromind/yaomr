ca866b8e81067f43dab9f1382cb5253751ae5ab9
Martin Vassor <martin.vassor@epfl.ch>
2018-02-24 03:01:27 +0100

    Remove useless lines

--- src/separator.py
+++ src/separator.py
@@ -21,32 +21,26 @@ print(output)
 if not os.path.exists(output):
     os.makedirs(output)
 
+threshold = 0.25 * myImage.size[1]
+myImage = myImage.convert("1")
 print "the image is: "
 print(myImage.format, myImage.size, myImage.mode)
-threshold = 0.25 * myImage.size[1]
-print "window width: " + str(window_width) + ", threshold: " + str(threshold)
-scaled = myImage.copy()
-#scaled.thumbnail((myImage.size[0], 48))
-scaled = scaled.convert("1")
-scaled.show()
-print "The thumbnail is:"
-print(scaled.format, scaled.size, scaled.mode)
 
 cur_sum = 0
 increasing=False
 next_open_slot=0
-for i in range(scaled.size[0]):
+for i in range(myImage.size[0]):
     prev_sum = cur_sum
     cur_sum = 0
-    for j in range(scaled.size[1]):
-        if scaled.getpixel((i, j)) < 64:
+    for j in range(myImage.size[1]):
+        if myImage.getpixel((i, j)) < 64:
             cur_sum = cur_sum + 1
     print cur_sum
     if cur_sum > prev_sum:
         increasing=True
     else:
         if increasing == True and prev_sum > threshold and i >= next_open_slot:
-            small = scaled.crop((i-window_width/2, 0, i + window_width/2, scaled.size[1]))
+            small = myImage.crop((i-window_width/2, 0, i + window_width/2, myImage.size[1]))
             small.save(output + "/" + name_file + "_" +str(i).zfill(3) + ".png")
             next_open_slot=i+immune
 
